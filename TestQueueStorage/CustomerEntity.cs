389ef54ccb7acfc4a560535a9b611fb1386b5741
Marco Antonino <antoninom@AULE.IT>
2017-06-29 18:06:09 +0200

    corretti alcuni errori

--- TestQueueStorage/CustomerEntity.cs
+++ TestQueueStorage/CustomerEntity.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace TestQueueStorage
 {
@@ -19,5 +21,7 @@ namespace TestQueueStorage
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
+        public string PartitionKey { get; private set; }
+        public string RowKey { get; private set; }
     }
 }
