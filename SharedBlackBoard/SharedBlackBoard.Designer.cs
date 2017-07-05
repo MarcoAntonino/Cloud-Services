711ab9fde3e2a92851ebc6797710582e9610e7ab
Marco Antonino <antoninom@AULE.IT>
2017-07-05 18:09:09 +0200

    lavorato con queueplus

---
+++ SharedBlackBoard/SharedBlackBoard.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SharedBlackBoard
+{
+    partial class SharedBlackBoard
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictBlckBrd = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.chkSend = new System.Windows.Forms.CheckBox();
+            this.chkReceive = new System.Windows.Forms.CheckBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBlckBrd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictBlckBrd
+            // 
+            this.pictBlckBrd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictBlckBrd.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictBlckBrd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictBlckBrd.Location = new System.Drawing.Point(12, 12);
+            this.pictBlckBrd.Name = "pictBlckBrd";
+            this.pictBlckBrd.Size = new System.Drawing.Size(963, 480);
+            this.pictBlckBrd.TabIndex = 0;
+            this.pictBlckBrd.TabStop = false;
+            this.pictBlckBrd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictBlckBrd_MouseDown);
+            this.pictBlckBrd.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictBlckBrd_MouseMove);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnClear.Location = new System.Drawing.Point(12, 498);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // chkSend
+            // 
+            this.chkSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkSend.AutoSize = true;
+            this.chkSend.Location = new System.Drawing.Point(325, 503);
+            this.chkSend.Name = "chkSend";
+            this.chkSend.Size = new System.Drawing.Size(51, 17);
+            this.chkSend.TabIndex = 3;
+            this.chkSend.Text = "Send";
+            this.chkSend.UseVisualStyleBackColor = true;
+            this.chkSend.CheckedChanged += new System.EventHandler(this.chkSend_CheckedChanged);
+            // 
+            // chkReceive
+            // 
+            this.chkReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkReceive.AutoSize = true;
+            this.chkReceive.Location = new System.Drawing.Point(411, 503);
+            this.chkReceive.Name = "chkReceive";
+            this.chkReceive.Size = new System.Drawing.Size(66, 17);
+            this.chkReceive.TabIndex = 4;
+            this.chkReceive.Text = "Receive";
+            this.chkReceive.UseVisualStyleBackColor = true;
+            this.chkReceive.CheckedChanged += new System.EventHandler(this.chkReceive_CheckedChanged);
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(132, 503);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(55, 13);
+            this.lblColor.TabIndex = 5;
+            this.lblColor.Text = "Use color:";
+            this.lblColor.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(194, 500);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 6;
+            this.txtColor.Text = "#FF0000";
+            // 
+            // SharedBlackBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 577);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.chkReceive);
+            this.Controls.Add(this.chkSend);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pictBlckBrd);
+            this.Name = "SharedBlackBoard";
+            this.Text = "Shared BlackBoard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictBlckBrd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBlckBrd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox chkSend;
+        private System.Windows.Forms.CheckBox chkReceive;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.TextBox txtColor;
+    }
+}
+
---
+++ SharedBlackBoard/SharedBlackBoard.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SharedBlackBoard
+{
+    public partial class SharedBlackBoard : Form
+    {
+        public SharedBlackBoard()
+        {
+            InitializeComponent();
+        }
+
+        int oldX, oldY;
+        string connectionString = "Endpoint=sb://avsbtest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=W504rGVBUid1FkOSVj3s8eYyO5NT1AhYZm/afvpXEIM=";
+        string queueName = "myTestSBqueue";
+        /*Queste due ultime variabili sono state fornite da Azure.
+         * Bisognerebbe metterle in file di config e magari criptarle
+         */
+        QueueClient ReceiveClient;
+        List<LineEntity> Lines = new List<LineEntity>();         
+
+        private void pictBlckBrd_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                DrawLine(e.X, e.Y, oldX, oldY, txtColor.Text);
+                
+
+                if (chkSend.Checked)
+                {
+                    LineEntity le = new LineEntity();
+                    le.x1 = e.X;
+                    le.y1 = e.Y;
+                    le.x2 = oldX;
+                    le.y2 = oldY;
+                    le.color = txtColor.Text;
+                    Lines.Add(le);
+                    if (Lines.Count==50)
+                    {
+                        SendLines(Lines);
+                        Lines.Clear();
+                    }
+                    //SendLine(e.X, e.Y, oldX, oldY, txtColor.Text);
+                }
+                oldX = e.X;
+                oldY = e.Y;
+
+            }
+        }
+
+        private void SendLines(List<LineEntity> lines)
+        {
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            StringBuilder sb = new StringBuilder();
+            foreach (LineEntity le in lines)
+            {
+                sb.Append(le.x1.ToString() + ",");
+                sb.Append(le.y1.ToString() + ",");
+                sb.Append(le.x1.ToString() + ",");
+                sb.Append(le.y2.ToString() + ",");
+                sb.Append(le.color.ToString() + ";");
+            }
+            BrokeredMessage message = new BrokeredMessage(sb.ToString());
+            client.Send(message);
+        }
+
+        private void SendLine(int x1, int y1, int x2, int y2, string color)
+        {
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            //è la coda di service bus. Questo queue client lo useremo andando a prendere una stringa di connessione
+            BrokeredMessage message = new BrokeredMessage(x1.ToString() + "," + y1.ToString() + "," + x2.ToString() + "," + y2.ToString() + "," + color);
+            client.Send(message);
+        }
+
+        private void DrawLine(int x1, int y1, int x2, int y2, string color)
+        {
+            Color c = System.Drawing.ColorTranslator.FromHtml(color);
+            Graphics g = pictBlckBrd.CreateGraphics();
+            g.DrawLine(new Pen(c,2), x1, y1, x2, y2);
+            g.Dispose();
+            /*Se io no nmettessi dispose, la variabile g uscirebbe dal suo scope e diventerebbe garbage e quindi finirebbe nel garbage collector ma data la pigrizia del GC, è meglio sganciare subito la risorsa fisica subito. Se non lo mettessi, sarebbe poi compito dei GC liberare la memoria occupata dall'oggetto ma in un momento determinato a priori. Con il rischio di saturare la memoria grafica.
+             * Si potrebbe wrappare nello using anche.
+             */
+            
+        }
+
+        private void chkSend_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSend.Checked)
+            {
+                chkReceive.Checked = false; //fai occhio che l'evento di default, cioè cambia lo status, si scatena
+            }
+        }
+
+        private void chkReceive_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkReceive.Checked)
+            {
+                chkSend.Checked = false;
+                ReceiveClient = QueueClient.CreateFromConnectionString(connectionString, queueName);
+                ReceiveClient.OnMessage(message =>
+                {
+                //string msg = message.GetBody<String>();
+                //int x1 = int.Parse(msg.Split(',')[0]);
+                //int y1 = int.Parse(msg.Split(',')[1]);
+                //int x2 = int.Parse(msg.Split(',')[2]);
+                //int y2 = int.Parse(msg.Split(',')[3]);
+                //string color = msg.Split(',')[4];
+                //DrawLine(x1, y1, x2, y2, color);
+
+                string msg = message.GetBody<String>();
+                string[] segs = msg.Split(';');
+                    foreach (string seg in segs)
+                    {
+                        if (seg.Length>0)
+                        {
+                            int x1 = int.Parse(seg.Split(',')[0]);
+                            int y1 = int.Parse(seg.Split(',')[1]);
+                            int x2 = int.Parse(seg.Split(',')[2]);
+                            int y2 = int.Parse(seg.Split(',')[3]);
+                            string color = seg.Split(',')[4];
+                            DrawLine(x1, y1, x2, y2, color);
+                        }
+                        
+                    }
+                    
+
+                });
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Graphics g;
+            g = pictBlckBrd.CreateGraphics();
+            g.Clear(Color.Black);
+            g.Dispose();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+            * Due parametri
+            * semnder: l'oggetto che ha generato l'evento
+            * eventArgs: i dati che quel tipo di eventi che si porta dietro. E sono specifici! Dei mouse eventArgs
+            */
+        private void pictBlckBrd_MouseDown(object sender, MouseEventArgs e)
+           
+        {
+            if (e.Button == MouseButtons.Left) //.Left è un enum
+            {
+                oldX = e.X;
+                oldY = e.Y;
+            }
+
+        }
+    }
+}
