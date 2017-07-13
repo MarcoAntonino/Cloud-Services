cbe99b50c896d810d123ef40963fd4eee832bf33
Marco Antonino <antoninom@AULE.IT>
2017-07-13 16:55:42 +0200

    fatti altri esempi

---
+++ ComputerVisionAPIsample/TestComputerVision.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ComputerVisionAPIsample
+{
+    partial class TestComputerVision
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblImage = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.picPicture = new System.Windows.Forms.PictureBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Location = new System.Drawing.Point(12, 15);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(36, 13);
+            this.lblImage.TabIndex = 0;
+            this.lblImage.Text = "Image";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(53, 12);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(532, 20);
+            this.txtFilename.TabIndex = 1;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(591, 9);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // picPicture
+            // 
+            this.picPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picPicture.Location = new System.Drawing.Point(12, 38);
+            this.picPicture.Name = "picPicture";
+            this.picPicture.Size = new System.Drawing.Size(573, 345);
+            this.picPicture.TabIndex = 3;
+            this.picPicture.TabStop = false;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(591, 38);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(252, 339);
+            this.txtResult.TabIndex = 4;
+            // 
+            // TestComputerVision
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 395);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.picPicture);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.lblImage);
+            this.Name = "TestComputerVision";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblImage;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.PictureBox picPicture;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ ComputerVisionAPIsample/TestComputerVision.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerVisionAPIsample
+{
+    public partial class TestComputerVision : Form
+    {
+        public TestComputerVision()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap picture;
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+            MakeRequest(txtFilename.Text);
+            picture = new Bitmap(txtFilename.Text);
+            picPicture.Image = picture;
+        }
+
+        /*
+         * La keyword async lo fa diventare come metodo assincrono la cui invocazione può avvenire su un thread parallelo, e chi l'ha lanciato, non aspetta il risultato
+         */
+        async void MakeRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+            /*
+             * Ha una serie d imetodi che permette di fare una chiamata http e ottenere una serie di metodi e ritornarli
+             */
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key","551f3c06990e4351af7eb53605c71c86");
+
+            string requestParameters ="visualFeatures=Categories,Description,Color&language=en";
+
+            // Assemble the URI for the REST API Call.
+
+            string uri = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze?"+ requestParameters;
+
+            /*
+             * Questo URL inizia con una stringa dal datacenter da cui ho creato la connessione
+             */
+            HttpResponseMessage response;
+            string responseContent;
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+            /*
+             * Fondamentalmente scrivere using è come dire: quel blocco di codice avrà la garanzia che subisca il dispose quando incontra l'unica graffa. E quindi chiude la connessione.
+             * Senza dispose, la connessione resta appesa e prima o poi potrebbe saturarsi.
+             * L'using o il dispose è molt oimportante quando utilizzi delle variabili che utilizzano delle risorse fisiche.
+             */
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream"); //sta arrivando uno stream di byte, bisogna specificare sempre cosa mandi
+                response = await client.PostAsync(uri, content);
+                //in questo caso sto per chiamare un metodo asincrono ma uso la parola chiave await che ne aspetta la risposta
+                //se non avessi messo il wait, questa response sarebbe andata avanti
+                responseContent = response.Content.ReadAsStringAsync().Result;
+            }
+
+            setResultText(responseContent);
+        }
+
+        private void setResultText(string txt)
+        {
+            if (txtResult.InvokeRequired)//propertie che dice se aseonda del contesto, le eventual iazion isu quel soggetto sono dirette o boh...
+            {
+                txtResult.Invoke(
+                    new Action(() => txtResult.Text = txt));
+                return;
+            }
+            txtResult.Text = txt;
+        }
+
+        private byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
